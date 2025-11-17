cc684b3650526e5e92a2b756912b5a17656bf4cd
ShenTan0 <jsczlwy@qq.com>
2025-11-17 19:58:24 +0800

    change path_handler in windows

--- src/include/utils/path_handler.h
+++ src/include/utils/path_handler.h
@@ -15,9 +15,9 @@
 
 inline std::filesystem::path getExecutablePath() {
     #ifdef _WIN32
-        char path[MAX_PATH];
+        wchar_t path[MAX_PATH];
         GetModuleFileNameW(NULL, path, MAX_PATH);
-        return std::filesystem::path(path).parent_path();
+        return std::filesystem::path(path);
     #elif defined(__linux__)
         char path[PATH_MAX];
         ssize_t len = readlink("/proc/self/exe", path, PATH_MAX);
@@ -40,7 +40,7 @@ inline std::filesystem::path getExecutablePath() {
 
 #define GENERATE_GET_RESOURCE_PATH_FUNC(func_name, resource_subdir) \
     inline std::string func_name(std::string resource) { \
-        std::filesystem::path executable_path = getExecutablePath(); \
+        std::filesystem::path executable_path = getExecutablePath().parent_path(); \
         return (executable_path / resource_subdir / resource).string(); \
     }
 
