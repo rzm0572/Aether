52941be4007508b35118ddd9ac34de0b96f7537a
silvermilight <silvermilight@gmail.com>
2025-12-26 20:33:40 +0800

    fix: fix BulletManager

--- src/include/system/bullet.h
+++ src/include/system/bullet.h
@@ -14,61 +14,48 @@ struct Bullet {
 
     bool collided { false };
     float shoot_time { 0.0f };
-
-    Bullet(BulletType type, glm::vec3 position, glm::vec3 velocity, float shoot_time) : type(type), position(position), velocity(velocity), collided(false), shoot_time(shoot_time) {}
 };
 
 class BulletManager {
 public:
     BulletManager() {
-        bullets_.reserve(capacity_);
+        bullets_.reserve(MAX_BULLETS);
     }
     
     void fire(BulletType type, glm::vec3 position, glm::vec3 velocity, float now) {
-        if (count_ >= MAX_BULLETS) {
+        if (bullets_.size() >= MAX_BULLETS) {
             return;
         }
 
-        while (count_ >= capacity_) {
-            expand();
-        }
-
         bullets_.emplace_back(
-            type, position, velocity, now
+            type, position, velocity, false, now
         );
-        count_++;
+    }
+
+    std::vector<Bullet>& getBullets() {
+        return bullets_;
     }
 
     void cleanBullets(float now) {
-        std::vector<Bullet> bullets_remaining;
-        bullets_remaining.reserve(capacity_);
-        for (int i = 0; i < count_; ++i) {
+        size_t alive_bullets = 0;
+        for (int i = 0; i < bullets_.size(); ++i) {
             if (now - bullets_[i].shoot_time > MAX_LIFE_TIME) {
                 continue;
             }
             if (bullets_[i].collided) {
                 continue;
             }
-            bullets_remaining.emplace_back(bullets_[i]);
+
+            bullets_[alive_bullets] = bullets_[i];
+            alive_bullets++;
         }
 
-        bullets_.swap(bullets_remaining);
+        bullets_.resize(alive_bullets);
     }
 
 private:
-    static constexpr int MAX_BULLETS = 16384;
+    static constexpr size_t MAX_BULLETS = 16384;
     static constexpr float MAX_LIFE_TIME = 20.0f;
 
-    void expand() {
-        if (capacity_ * 2 > MAX_BULLETS) {
-            return;
-        }
-
-        capacity_ = capacity_ * 2;
-        bullets_.reserve(capacity_);
-    }
-
     std::vector<Bullet> bullets_;
-    int capacity_ { 128 };
-    int count_ { 0 };
 };
